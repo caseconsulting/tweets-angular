9687cebaabd45f311ac7d94d0750c8873d663e29
Jacob <jacobboisseau@gmail.com>
2017-06-21 12:45:14 -0400

    adding-old-content: made app handle loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'search-bar',
+  selector: 'app',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
