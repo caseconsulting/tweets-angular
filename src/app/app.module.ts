c375118438801aa4a12d2866a3c3ce3bdb29e5c5
Fahad <fahada95@vt.edu>
2017-06-21 12:51:54 -0400

    added output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { SearchBarComponent } from './search-bar/search-bar.component';
+
+import { OutputComponent } from './output/output.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    OutputComponent
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/output/output.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'output',
+  templateUrl: './output.component.html',
+  styleUrls: ['./output.component.css']
+})
+
+export class OutputComponent{
+   title = 'output works';
+}
